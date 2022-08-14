bc6166cf6eb0ae64af143de020b1ef779ee88841
Deopoler <seonjin6804@gmail.com>
2022-08-14 21:08:37 +0900

    change type alias

--- Implementation/Graph/Shortest Path/bellman_ford.cpp
+++ Implementation/Graph/Shortest Path/bellman_ford.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using vvi = vector<tuple<int, int, int>>;
+using vtiii = vector<tuple<int, int, int>>;
 using vll = vector<long long>;
 
-bool bellman_ford(vvi &graph, vll &distance, int start, int nodes)
+bool bellman_ford(vtiii &graph, vll &distance, int start, int nodes)
 {
     distance[start] = 0;
     for (int i = 0; i < nodes - 1; i++)
