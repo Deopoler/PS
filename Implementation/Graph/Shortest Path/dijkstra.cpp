e10d586eb4dce124561a528d454a53dc1fa8f6f9
Deopoler <seonjin6804@gmail.com>
2022-08-27 15:37:36 +0900

    Add adjacency matrix dijkstra

--- Implementation/Graph/Shortest Path/dijkstra.cpp
+++ Implementation/Graph/Shortest Path/dijkstra.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define llinf (numeric_limits<long long>::max() / 4)
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int)x.size())
 
-using vvi = vector<vector<pair<int, int>>>;
+// 인접리스트 구현
+using ll = long long;
+using vvil = vector<vector<pair<int, long long>>>;
 using vll = vector<long long>;
-
-void dijkstra(vvi &graph, vll &distance, int start)
+void dijkstra(vvil &graph, vll &distance, int start)
 {
-    priority_queue<pair<int, int>> q;
+    priority_queue<pair<ll, int>> q;
 
     distance[start] = 0;
     q.push({0, start});
@@ -20,6 +24,47 @@ void dijkstra(vvi &graph, vll &distance, int start)
             {
                 distance[next] = distance[node] + w;
                 q.push({-distance[next], next}); // c++ priority queue는 최대 힙이기 때문에 거리에 -를 붙임
+            }
+        }
+    }
+}
+
+// 인접행렬 구현
+using ll = long long;
+using vvll = vector<vector<long long>>;
+using vll = vector<long long>;
+void dijkstra(vvll &graph, vll &distance, int start)
+{
+    vector<bool> visited(graph.size(), false);
+    visited[start] = true;
+    for (int i = 0; i < sz(graph); i++)
+    {
+        distance[i] = graph[start][i];
+    }
+
+    int n = sz(graph);
+    while (n--)
+    {
+        int current = 0;
+        int min = llinf;
+        for (int i = 0; i < sz(graph); i++) // 최소비용 노드 찾기
+        {
+            if (distance[i] < min && !visited[i])
+            {
+                min = distance[i];
+                current = i;
+            }
+        }
+        visited[current] = true;
+
+        for (int i = 0; i < sz(graph); i++)
+        {
+            if (!visited[i])
+            {
+                if (distance[current] + graph[current][i] < distance[i])
+                {
+                    distance[i] = distance[current] + graph[current][i];
+                }
             }
         }
     }
