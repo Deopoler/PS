8171f94b09ed4455d014895ae655a396e31e1ae0
Deopoler <seonjin6804@gmail.com>
2022-09-05 21:22:20 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Macros */
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) ((int)x.size())
+#define eps 1e-9
+constexpr int inf = numeric_limits<int>::max() / 2;
+constexpr long long llinf = numeric_limits<long long>::max() / 2;
+
+/* Types Aliases */
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using ld = long double;
+#define umap unordered_map
+#define uset unordered_set
+#define ummap unordered_multimap
+#define umset unordered_multiset
+#define mmap multimap
+#define mset multiset
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vii = vector<pair<int, int>>;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+
+/* Utils */
+template <typename T>
+struct reversion_wrapper
+{
+    T &iterable;
+};
+
+template <typename T>
+auto begin(reversion_wrapper<T> w) { return std::rbegin(w.iterable); }
+
+template <typename T>
+auto end(reversion_wrapper<T> w) { return std::rend(w.iterable); }
+
+template <typename T>
+reversion_wrapper<T> reversed(T &&iterable) { return {iterable}; }
+/* End Template */
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
